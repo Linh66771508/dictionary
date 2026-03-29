b03acd2b884c4d68c4d7b5345c8cce4d9d92be02
Linh <phuonglinh66771508@gmail.com>
2026-03-29 20:02:53 +0700

    Remove unused design reference

--- Dyar Bakr Dictionary (Community)/src/app/App.tsx
+++
@@ -1,9 +0,0 @@
-import { DictionaryApp } from '@/app/components/DictionaryApp';
-
-export default function App() {
-  return (
-    <div className="size-full">
-      <DictionaryApp />
-    </div>
-  );
-}
